cb61d4c6d7b458c6ef98fcb7fb2c42ee1ae492aa
TAKATAMA Hirokazu <takatama@air-tama.local>
2015-01-23 01:04:30 +0900

    withAlert must use assert if it checks alert text.

--- src/test/groovy/hello/StatusSpec.groovy
+++ src/test/groovy/hello/StatusSpec.groovy
@@ -3,14 +3,16 @@ package hello
 import geb.spock.GebSpec
 
 class StatusSpec extends GebSpec {
-    def "寝ているのを起こす"() {
+    def "寝る子を起こして眠らせる"() {
         setup:
             to StatusPage
-        expect:
-            status.text() == "Sleeping..."
         when:
-            getupButton.click()
+            assert withAlert { getupButton.click() } == "Getting up!"
         then:
             status.text() == "Getting up!"
+        when:
+            assert withAlert { sleepButton.click() } == "Sleeping..."
+        then:
+            status.text() == "Sleeping..."
     }
 }
